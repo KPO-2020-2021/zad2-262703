22303941692f6f6de67c5f17750dd13c70056b6e
Raman Skuratovich <262703@student.pwr.edu.pl>
2021-03-30 12:19:26 +0200

    Delete

--- inc/stat.hh
+++
@@ -1,8 +0,0 @@
-#ifndef STAT_HH
-#define STAT_HH
-#include <iostream>
-using namespace std;
-
-void Statystyka(int good, int bad);
-
-#endif
--- src/stat.cpp
+++
@@ -1,9 +0,0 @@
-#include "stat.hh"
-
-void Statystyka(int good, int bad){
-    double Proc;
-    Proc = ((double)good/((double)good+(double)bad))*100;
-    cout << "Ilosc dobrych  odpowiedzi: " << good << endl;
-    cout << "Ilosc blednych odpowiedzi: " << bad << endl;
-    cout << "Wynik procentowy poprawnych odpowiedzi:    " << Proc << "%" << endl;
-}
